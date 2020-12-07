d15325fdab68ff7ab66d148c5906fd8cf31f87e5
Jan-Pieter van den Heuvel <jan-pieter@piozum.com>
2020-12-07 06:39:24 +0100

    Problem 2017-04

---
+++ 2017/src/main/scala/Problem04.scala
@@ -0,0 +1,15 @@
+import scala.io.Source
+
+object Problem04 extends App {
+
+  val input = Source.fromResource("04-input.txt").getLines().toList.map(_.split(" "))
+
+  val withoutDuplicates = input.filter(words => words.length == words.distinct.length)
+
+  println(withoutDuplicates.size)
+
+  val withoutAnagrams = input.filter(words => words.length == words.map(word => word.toList.groupBy(c => c).mapValues(_.size)).toSet.size)
+
+  println(withoutAnagrams.size)
+
+}
