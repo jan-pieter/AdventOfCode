d4b185679c28d862d06e33fa1c9a55f967fe329e
Jan-Pieter van den Heuvel <jan-pieter@piozum.com>
2022-12-05 09:13:38 +0100

    Fix scripts

--- downloadInput.sh
+++ downloadInput.sh
@@ -2,10 +2,9 @@
 
 YEAR=2022
 DAY=5
-SESSION=53616c7465645f5fbe656246a8867f24f56ec389a48a4b892c949dee2a170da2d2cd50e222e1540133317f7d745a9fff9a73392c979edc18f25ed811e792b764
 
 formatted_day=$(printf "%02d" $DAY)
 
 echo "Downloading to ./$YEAR/src/main/resources/$formatted_day-input.txt"
 
-curl -H "Cookie: session=$SESSION;" -o "./$YEAR/src/main/resources/$formatted_day-input.txt" "https://adventofcode.com/$YEAR/day/$DAY/input"
+curl -H "Cookie: session=$SESSION;"  -A "jan-pieter via curl" -o "./$YEAR/src/main/resources/$formatted_day-input.txt" "https://adventofcode.com/$YEAR/day/$DAY/input"
--- downloadLeaderboard.sh
+++ downloadLeaderboard.sh
@@ -2,7 +2,6 @@
 
 BOARD=400015
 YEAR=2022
-SESSION=53616c7465645f5fbe656246a8867f24f56ec389a48a4b892c949dee2a170da2d2cd50e222e1540133317f7d745a9fff9a73392c979edc18f25ed811e792b764
 
 filename="./leaderboard/$BOARD-$YEAR.js"
 
@@ -10,6 +9,6 @@ echo "Downloading to $filename"
 
 echo 'callBack(' > $filename
 
-curl -H "Cookie: session=$SESSION;" -o - "https://adventofcode.com/$YEAR/leaderboard/private/view/$BOARD.json" >> $filename
+curl -H "Cookie: session=$SESSION;" -A "jan-pieter via curl" -o - "https://adventofcode.com/$YEAR/leaderboard/private/view/$BOARD.json" >> $filename
 
 echo ')' >> $filename
