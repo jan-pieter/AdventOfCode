3604b2384d8b893abeb6040cb8d4e0f437bb1d0f
Jan-Pieter van den Heuvel <jan-pieter@piozum.com>
2022-12-21 09:59:02 +0100

    Problem 2022 20 part 2

--- 2022/src/main/scala/Problem20.scala
+++ 2022/src/main/scala/Problem20.scala
@@ -1,40 +1,45 @@
 import scala.io.Source
 
 object Problem20 extends App:
-  case class Number(value: Int, hasMoved: Boolean)
-  val input: Vector[Number] = Source.fromResource("20-input.txt").getLines().toVector.map(i => Number(i.toInt, false))
+  case class Number(value: Long, originalIndex: Int)
+  val input: Vector[Number] = Source.fromResource("20-input.txt").getLines().toVector.zipWithIndex.map((i, index) => Number(i.toLong, index))
   println(input.mkString(" "))
-  // Mix
-  var mixed: Vector[Number] = input
-  var next = mixed.indexWhere(!_.hasMoved)
-  while(next >= 0) {
-//    println(s"next $next with val ${mixed(next).value}")
-    val element = mixed(next)
-    val newRawIndex = next + (element.value % (mixed.length - 1))
-    val newIndex = {
-      if (newRawIndex > 0 && newRawIndex < mixed.length) newRawIndex
-      else if (newRawIndex > 0) newRawIndex % (mixed.length - 1)
-      else mixed.length + newRawIndex - 1
+  def mix(prev: Vector[Number]): Vector[Number] = {
+    // Mix
+    var mixed: Vector[Number] = prev
+    input.indices.foreach { index =>
+//      val next = mixed.indexOf(Number(input(index).value, index))
+      val next = mixed.indexWhere(_.originalIndex == index)
+      //    println(s"next $next with val ${mixed(next).value}")
+      val element = mixed(next)
+      val newRawIndex = next + (element.value % (mixed.length - 1)).toInt
+      val newIndex = {
+        if (newRawIndex > 0 && newRawIndex < mixed.length) newRawIndex
+        else if (newRawIndex > 0) newRawIndex % (mixed.length - 1)
+        else mixed.length + newRawIndex - 1
+      }
+      //    val newIndex = (next + element.value) % (mixed.length - 1)
+      //    println(s"newIndex $newIndex")
+      val withoutElement = mixed.patch(next, Nil, 1)
+      if (newIndex <= next) {
+        mixed = (mixed.take(newIndex) :+ element) ++ withoutElement.drop(newIndex)
+      } else {
+        mixed = (withoutElement.take(newIndex) :+ element) ++ withoutElement.drop(newIndex)
+      }
+      //    println(mixed.map(_.value).mkString(" "))
     }
-//    val newIndex = (next + element.value) % (mixed.length - 1)
-//    println(s"newIndex $newIndex")
-    val withoutElement = mixed.patch(next, Nil, 1)
-    if (newIndex <= next) {
-      mixed = (mixed.take(newIndex) :+ element.copy(hasMoved = true)) ++ withoutElement.drop(newIndex)
-    } else {
-      mixed = (withoutElement.take(newIndex) :+ element.copy(hasMoved = true)) ++ withoutElement.drop(newIndex)
-    }
-//    println(mixed.map(_.value).mkString(" "))
-    next = mixed.indexWhere(!_.hasMoved)
-//    next = -1
+    mixed
   }
 
-  val index0 = mixed.indexOf(Number(0, true))
-  println(index0)
-  val solution1 = List(1000, 2000, 3000).map(_ + index0).map(_ % mixed.length).map(mixed).map(_.value)
-//  solution1.foreach(println(_))
-  println(solution1.sum)
+  def printSolution(mixed: Vector[Number]): Unit = {
+    val index0 = mixed.indexWhere(_.value == 0)
+    println(index0)
+    val solution = List(1000, 2000, 3000).map(_ + index0).map(_ % mixed.length).map(mixed).map(_.value)
+    println(solution.sum)
+  }
 
+  val mixed = mix(input)
+  printSolution(mixed)
 
-//  val solution2 = input.map(_.sum).sorted.takeRight(3).sum
-//  println(solution2)
+  val mixed2 = (0 until 10).foldLeft(input.map(number => number.copy(number.value * 811589153)))((prev, _) => mix(prev))
+  printSolution(mixed2)
