bc3be7305c16137ac7cf50c858fa39af7ae365cc
Jan-Pieter van den Heuvel <jan-pieter@piozum.com>
2023-12-03 10:08:41 +0100

    Use isDigit

--- 2023/src/main/scala/Problem03.scala
+++ 2023/src/main/scala/Problem03.scala
@@ -2,8 +2,7 @@ import scala.io.Source
 
 object Problem03 extends App:
   val input = Source.fromResource("03-input.txt").getLines().toVector
-  def isDigit(c: Char): Boolean = c >= '0' && c <= '9'
-  def isSymbol(c: Char): Boolean = !isDigit(c) && c != '.'
+  def isSymbol(c: Char): Boolean = !c.isDigit && c != '.'
   def symbolAt(y: Int, x: Int): Boolean =
     Option.when(y >= 0 && y < input.length && x >= 0 && x < input(y).length)(isSymbol(input(y)(x))).getOrElse(false)
   def gearAt(y: Int, x: Int): Boolean =
@@ -12,9 +11,9 @@ object Problem03 extends App:
   val numbers = for {
     y <- input.indices
     x <- input(y).indices
-    if isDigit(input(y)(x)) && (x == 0 || input(y)(x-1) == '.' || isSymbol(input(y)(x-1)))
+    if input(y)(x).isDigit && (x == 0 || input(y)(x-1) == '.' || isSymbol(input(y)(x-1)))
   } yield {
-    val number = input(y).drop(x).takeWhile(isDigit)
+    val number = input(y).drop(x).takeWhile(_.isDigit)
     val hasSymbol = symbolAt(y, x-1) || symbolAt(y, x + number.length) || (x-1 to x+number.length).exists(x2 => symbolAt(y-1, x2) || symbolAt(y+1, x2))
     Option.when(hasSymbol)(number.toLong)
   }
@@ -24,9 +23,9 @@ object Problem03 extends App:
   val numbers2 = for {
     y <- input.indices
     x <- input(y).indices
-    if isDigit(input(y)(x)) && (x == 0 || input(y)(x - 1) == '.' || isSymbol(input(y)(x - 1)))
+    if input(y)(x).isDigit && (x == 0 || input(y)(x - 1) == '.' || isSymbol(input(y)(x - 1)))
   } yield {
-    val number = input(y).drop(x).takeWhile(isDigit)
+    val number = input(y).drop(x).takeWhile(_.isDigit)
     Option.when(gearAt(y, x-1))((y, x-1) -> number.toLong).toVector ++
       Option.when(gearAt(y, x+number.length))((y, x+number.length) -> number.toLong).toVector ++
       (x - 1 to x + number.length).filter(x2 => gearAt(y-1,x2)).map(x2 => (y-1, x2) -> number.toLong) ++
