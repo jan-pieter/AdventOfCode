4ee5ab6eb5f56209d3a2b24548f1f890d491a490
Jan-Pieter van den Heuvel <jan-pieter@piozum.com>
2024-12-09 22:48:18 +0100

    2024 day 9 refactor

--- 2024/src/main/scala/Problem09.scala
+++ 2024/src/main/scala/Problem09.scala
@@ -12,37 +12,39 @@ object Problem09 extends App:
   var i = 0
   var j = expanded.length-1
   while (i < j) {
-    if (result1(i) >= 0) then i = i+1
+    if result1(i) >= 0 then i = i+1
     else if result1(j) < 0 then j = j-1
     else
       val origI = result1(i)
       result1 = result1.updated(i, result1(j)).updated(j, origI)
   }
-  val answer1 = result1.zipWithIndex.foldLeft(0L) { case (acc, (c, i)) =>
+
+  def checksum(result: Vector[Int]): Long = result.zipWithIndex.foldLeft(0L) { case (acc, (c, i)) =>
     if c == -1 then acc else acc + i * c
   }
-  println(answer1)
+
+  println(checksum(result1))
 //  println(result1.mkString("").replace("-1", "."))
 
-  var filesLeft = input.zipWithIndex.filter((c, i) => i % 2 == 0).map((c, i) => (c.asDigit, i))
-  val result2 = input.zipWithIndex.foldLeft(Vector.empty[Int]){
-    case (acc, (c, i)) if i % 2 == 1 || filesLeft.forall((d, j) => j != i) =>
+  val initFilesLeft: Vector[(Int, Int)] = input.zipWithIndex.filter((c, i) => i % 2 == 0).map((c, i) => (c.asDigit, i))
+  val result2 = input.zipWithIndex.foldLeft((Vector.empty[Int], initFilesLeft)){
+    case ((acc, filesLeft), (c, i)) if i % 2 == 1 || filesLeft.forall((d, j) => j != i) =>
       @annotation.tailrec
-      def fillWithLastEligible(acc: Vector[Int], toFill: Int): Vector[Int] = {
+      def fillWithLastEligible(acc: Vector[Int], toFill: Int, filesLeft: Vector[(Int, Int)]): (Vector[Int], Vector[(Int, Int)]) = {
         val rightEligible = filesLeft.findLast((d, j) => d <= toFill && i < j)
         rightEligible match {
-          case None => acc ++ Vector.fill(toFill)(-1)
+          case None => (acc ++ Vector.fill(toFill)(-1), filesLeft)
           case Some((d, j)) =>
-            filesLeft = filesLeft.patch(filesLeft.lastIndexOf((d, j)), Nil, 1)
-            fillWithLastEligible(acc ++ Vector.fill(d)(j / 2), toFill - d)
+            fillWithLastEligible(
+              acc ++ Vector.fill(d)(j / 2),
+              toFill - d,
+              filesLeft.patch(filesLeft.lastIndexOf((d, j)), Nil, 1)
+            )
         }
       }
-      fillWithLastEligible(acc, c.asDigit)
-    case (acc, (c, i)) if i % 2 == 0 => acc ++ Vector.fill(c.asDigit)(i / 2)
+      fillWithLastEligible(acc, c.asDigit, filesLeft)
+    case ((acc, filesLeft), (c, i)) if i % 2 == 0 => (acc ++ Vector.fill(c.asDigit)(i / 2), filesLeft)
   }
 
-  val answer2 = result2.zipWithIndex.foldLeft(0L) { case (acc, (c, i)) =>
-    if c == -1 then acc else acc + i * c
-  }
-  println(answer2)
+  println(checksum(result2._1))
 //  println(result2.mkString("").replace("-1", "."))
