27de8a046376220da7fa12f4f8c2fe4fa99141a8
Jan-Pieter van den Heuvel <jan-pieter@piozum.com>
2024-12-15 07:30:26 +0100

    2024 day 14 cleanup

--- 2024/src/main/scala/Problem14.scala
+++ 2024/src/main/scala/Problem14.scala
@@ -31,55 +31,29 @@ object Problem14 extends App:
   val quadrants = after100.map(_.toQuadrant).foldLeft((0, 0, 0, 0)) {
     case ((q1, q2, q3, q4), (q1_, q2_, q3_, q4_)) => (q1+q1_, q2+q2_, q3+q3_, q4+q4_)
   }
-  val answer = (quadrants._1 * quadrants._2 * quadrants._3 * quadrants._4)
+  val answer = quadrants._1 * quadrants._2 * quadrants._3 * quadrants._4
   println(answer)
 
-//  println(robots)
-//  println(after100)
-//  println(quadrants)
-
-  def robotsToGrid(robots: Vector[Robot]) = {
+  def robotsToGrid(robots: Vector[Robot]) =
     val grid = Array.fill(height, width)('.')
     robots.foreach { robot =>
       grid(robot.y)(robot.x) = '#'
     }
     grid
-  }
-  def printGrid(grid: Array[Array[Char]]) = {
+
+  def printGrid(grid: Array[Array[Char]]) =
     grid.foreach { row =>
       println(row.mkString)
     }
-  }
-  def christmasTreeLike(grid: Array[Array[Char]]) = {
-    (1 to 50).forall{line =>
-      val prev = grid(line-1).count(_ == '#')
-      val curr = grid(line).count(_ == '#')
-      prev < (curr-2) || prev < (curr + 2)
-    }
-  }
 
-  def robotNeighbours(robots: Vector[Robot]): Int = {
-    robots.map(robot => robots.count(other => (robot.x - other.x).abs <= 1 && (robot.y - other.y).abs <= 1) - 1).sum
-  }
-  val result = (1 to width*height).toVector.map(steps => steps -> robots.map(_.positionAfter(steps))).maxBy((steps, robots) => robotNeighbours(robots))
-  println(result)
+  def robotNeighbours(robots: Vector[Robot]): Int =
+    robots.map(robot =>
+      robots.count(other => (robot.x - other.x).abs <= 1 && (robot.y - other.y).abs <= 1) - 1
+    ).sum
+
+  val result = (1 to width*height)
+    .toVector
+    .map(steps => steps -> robots.map(_.positionAfter(steps)))
+    .maxBy((steps, robots) => robotNeighbours(robots))
+  println(result._1)
   printGrid(robotsToGrid(result._2))
-
-  val margin = 5
-//  (1 to 1000).map(steps => steps -> robotsToGrid(robots.map(_.positionAfter(steps)))).filter((steps, grid) => christmasTreeLike(grid)).foreach{(steps, grid) => printGrid(grid); println(steps)}
-//  (1 to 100000).map(steps => steps -> robots.map(_.positionAfter(steps).toQuadrant).foldLeft((0, 0, 0, 0)) {
-//    case ((q1, q2, q3, q4), (q1_, q2_, q3_, q4_)) => (q1 + q1_, q2 + q2_, q3 + q3_, q4 + q4_)
-//  }).filter {
-    //case (steps, (q1, q2, q3, q4)) => q1 > q2-margin && q1 < q2+margin && q3 > q4-margin && q3 < q4+margin && q3 > q1 && q4 > q1
-//    case (steps, (q1, q2, q3, q4)) => q1 == q2 && q3 == q4 && q3 > q1 && q4 > q2
-//  }.filter {
-//    case (steps, _) => christmasTreeLike(robotsToGrid(robots.map(_.positionAfter(steps))))
-//    }
-//    .foreach {
-//    case (steps, _) =>
-//      printGrid(robotsToGrid(robots.map(_.positionAfter(steps))))
-//      println(steps)
-//  }
-  //printRobots(robots.map(_.positionAfter(9907)))
-
-
